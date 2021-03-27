a425b12491e821ff5a820c654c1081fb00292ec7
MrHonekawa <76059318+MrHonekawa@users.noreply.github.com>
2021-03-27 22:34:01 +0530

    Update gun_game.py

--- SaitamaRobot/modules/gun_game.py
+++ SaitamaRobot/modules/gun_game.py
@@ -1,1 +1,11 @@
+import html
+import random
+import SaitamaRobot.modules.gun_game_string as gun_game_string
+from SaitamaRobot import dispatcher
+from telegram import ParseMode, Update, Bot
+from telegram.ext import CallbackContext, run_async
 
+@run_async
+def guns_name2(update: Update, context: CallbackContext):
+  args = context.args
+  update.effective_message.reply_text(random.choice(gun_game_string.GUNS2))
